c528ce4e869553cd3b1cd340feed476ad6f91dc0
Mauro-Bertozzi11 <106584065+Mauro-Bertozzi11@users.noreply.github.com>
2023-03-20 15:42:37 +0100

    initial commit

---
+++ action.ps1
@@ -0,0 +1,106 @@
+# HelloID-Task-SA-Target-AzureActiveDirectory-GroupGrantOwnership
+##################################################################
+# Form mapping
+$formObject = @{
+    groupId     = $form.groupId
+    ownersToAdd = $form.ownersToAdd  
+}
+try {
+    Write-Information "Executing AzureActiveDirectory action: [GroupGrantOwnership] for: [$($formObject.groupId)]"
+    Write-Information "Retrieving Microsoft Graph AccessToken for tenant: [$AADTenantID]"
+    $splatTokenParams = @{
+        Uri         = "https://login.microsoftonline.com/$($AADTenantID)/oauth2/token"
+        ContentType = 'application/x-www-form-urlencoded'
+        Method      = 'POST'
+        Body        = @{                                                                                                                         
+            grant_type    = 'client_credentials'
+            client_id     = $AADAppID
+            client_secret = $AADAppSecret
+            resource      = 'https://graph.microsoft.com'
+        }
+    }
+    $accessToken = (Invoke-RestMethod @splatTokenParams).access_token
+
+    $headers = [System.Collections.Generic.Dictionary[string, string]]::new()
+    $headers.Add("Authorization", "Bearer $($accessToken)")
+    $headers.Add("Content-Type", "application/json")
+
+    foreach ($owner in $formObject.ownersToAdd){
+        try {
+            $splatAddOwnerToGroup = @{
+                Uri         = "https://graph.microsoft.com/v1.0/groups/$($formObject.groupId)/owners/`$ref"
+                ContentType = 'application/json'
+                Method      = 'POST'
+                Headers     = $headers
+                Body        = @{ '@odata.id' = "https://graph.microsoft.com/v1.0/users/$($owner.userPrincipalName)" } | ConvertTo-Json -Depth 10
+            }
+            $null = Invoke-RestMethod @splatAddOwnerToGroup
+
+            $auditLog = @{
+                Action            = 'UpdateResource'
+                System            = 'AzureActiveDirectory'
+                TargetIdentifier  = $formObject.groupId 
+                TargetDisplayName = $formObject.groupId
+                Message           = "AzureActiveDirectory action: [GroupGrantOwnership] for group [$($formObject.groupId)] for: [$($owner.userPrincipalName)] executed successfully"
+                IsError           = $false
+            }
+    
+            Write-Information -Tags 'Audit' -MessageData $auditLog
+            Write-Information $auditLog.Message
+        }
+        catch {
+            $ex = $_
+            if (-not[string]::IsNullOrEmpty($ex.ErrorDetails)) {
+                $errorExceptionDetails = ($_.ErrorDetails | ConvertFrom-Json).error.Message
+            }else {
+                $errorExceptionDetails = $ex.Exception.Message
+            }
+
+            if (($ex.Exception.Response) -and ($Ex.Exception.Response.StatusCode -eq 404)) {
+                # 404 indicates already removed
+                $auditLog = @{
+                    Action            = 'UpdateResource'
+                    System            = 'AzureActiveDirectory'
+                    TargetIdentifier  = $formObject.groupId
+                    TargetDisplayName = $formObject.groupId
+                    Message           = "AzureActiveDirectory action: [GroupGrantOwnership for group [$($formObject.groupId))] ] for: [$($owner.userPrincipalName)] executed successfully. Note that the account was not a member"
+                    IsError           = $false
+                }
+                Write-Information -Tags 'Audit' -MessageData $auditLog
+                Write-Information $auditLog.Message
+            } else{
+                $auditLog = @{
+                    Action            = 'UpdateResource'
+                    System            = 'AzureActiveDirectory'
+                    TargetIdentifier  = $formObject.groupId 
+                    TargetDisplayName = $formObject.groupId
+                    Message           = "Could not execute AzureActiveDirectory action: [GroupGrantOwnership] for group [$($formObject.groupId)] for: [$($owner.userPrincipalName)], error: $($errorExceptionDetails)"
+                    IsError           = $true
+                }
+                Write-Information -Tags "Audit" -MessageData $auditLog
+                Write-Error $auditLog.Message
+            }
+        }
+    }
+}
+catch {
+    $ex = $_
+    if (-not[string]::IsNullOrEmpty($ex.ErrorDetails)) {
+        $errorExceptionDetails = ($_.ErrorDetails | ConvertFrom-Json).error.Message
+    }else {
+        $errorExceptionDetails = $ex.Exception.Message
+    }
+
+    $auditLog = @{
+        Action            = 'UpdateResource'
+        System            = 'AzureActiveDirectory'
+        TargetIdentifier  = $formObject.groupId 
+        TargetDisplayName = $formObject.groupId
+        Message           = "Could not execute AzureActiveDirectory action: [GroupGrantOwnership] for: [$($formObject.groupId)], error: $($errorExceptionDetails)"
+        IsError           = $true
+    }
+    
+    Write-Information -Tags "Audit" -MessageData $auditLog
+    Write-Error "$($auditLog.Message)"
+}
+##################################################################
